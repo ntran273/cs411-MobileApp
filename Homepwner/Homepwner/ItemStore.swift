e95cf154ece22fda2ae66bb14ab1fdee3ddcb2b5
Nguyen Tran <ntran272@csu.fullerton.edu>
2018-02-12 21:01:39 -0800

    updated

--- Homepwner/Homepwner/ItemStore.swift
+++ Homepwner/Homepwner/ItemStore.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2018 Nguyen Tran. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ItemStore{
+    var allItems = [Item]()
+    
+    init(){
+        for _ in 0..<5 {
+            createItem()
+        }
+    }
+    
+    
+    @discardableResult func createItem() -> Item{
+        let newItem = Item(random: true)
+        
+        allItems.append(newItem)
+        return newItem
+    }
+}
